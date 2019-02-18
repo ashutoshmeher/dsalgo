dc17dac873a937ba47f1e3c8ca6408697d86bef4
Ashutosh Meher <ashutosh.meher@tothenew.com>
2019-02-18 15:49:40 +0530

    Insertion Sort

--- src/main/java/code/ashutoshmeher/dsalgo/sorting/InsertionSort.java
+++ src/main/java/code/ashutoshmeher/dsalgo/sorting/InsertionSort.java
@@ -3,6 +3,23 @@ package code.ashutoshmeher.dsalgo.sorting;
 public class InsertionSort {
 
     public static int[] sort(int[] input){
+
+        for(int i=1;i<input.length;i++){
+
+            for(int j=0;j<i;j++){
+                if(input[i] < input[j]){
+                    int tmp = input[i];
+                    shift(j, i, input);
+                    input[j] = tmp;
+                }
+            }
+        }
         return input;
+    }
+
+    private static void shift(int from, int to, int[] input){
+        for(int i=to;i>from;i--){
+            input[i] = input[i-1];
+        }
     }
 }
